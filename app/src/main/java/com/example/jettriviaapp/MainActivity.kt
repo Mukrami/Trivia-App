014a15387147639756a0408997725a2428559849
Mukrami <138502133+Mukrami@users.noreply.github.com>
2023-07-24 12:23:39 +0800

    Commit Final

--- app/src/main/java/com/example/jettriviaapp/MainActivity.kt
+++ app/src/main/java/com/example/jettriviaapp/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
             JetTriviaAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-//                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     TriviaHome()
